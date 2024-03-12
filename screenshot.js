1b800a1bee67ecfd91aff52d561c9ccc4383472b
Cassidy Williams <cassidoo@users.noreply.github.com>
2024-03-12 10:43:57 -0500

    Add date to PDF footer

--- screenshot.js
+++ screenshot.js
@@ -1,22 +1,38 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-  let website = 'https://cosynd.com';
+	let website = "https://cosynd.com";
 
-  if (process.argv.length > 2) {
-    website = process.argv[2];
-  }
+	if (process.argv.length > 2) {
+		website = process.argv[2];
+	}
 
-  await page.goto(website, { waitUntil: 'networkidle0' });
+	await page.goto(website, { waitUntil: "networkidle0" });
 
-  await page.setViewport({ width: 1920, height: 1080 });
+	await page.setViewport({ width: 1920, height: 1080 });
 
-  await page.screenshot({ path: 'screenshot.png' });
-  await page.pdf({ path: 'screenshot.pdf' });
+	await page.screenshot({ path: "screenshot.png", fullPage: true });
 
-  await browser.close();
+	await page.pdf({
+		path: "screenshot.pdf",
+		scale: 0.75,
+		displayHeaderFooter: true,
+		footerTemplate: `
+      <div style="color: lightgray; font-size: 10px; padding-top: 5px; text-align: center; width: 100%;">
+        <span class="date"></span> - <span class="url"></span>
+      </div>`,
+		margin: {
+			bottom: 30,
+			left: 10,
+			right: 10,
+			top: 10,
+		},
+		printBackground: true,
+		landscape: true,
+	});
+
+	await browser.close();
 })();
-
